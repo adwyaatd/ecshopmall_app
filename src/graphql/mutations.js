9da2ad02f043fea71f8c700b5514ff361bdf1ebd
細田来夢 <52363465+adwyaatd@users.noreply.github.com>
2021-12-30 21:26:20 +0900

    amplify,graphQLを追加

---
+++ src/graphql/mutations.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createESM = /* GraphQL */ `
+  mutation CreateESM($input: CreateESMInput!) {
+    createESM(input: $input) {
+      Data
+      PK
+      SK
+    }
+  }
+`;
+export const updateESM = /* GraphQL */ `
+  mutation UpdateESM($input: UpdateESMInput!) {
+    updateESM(input: $input) {
+      Data
+      PK
+      SK
+    }
+  }
+`;
+export const deleteESM = /* GraphQL */ `
+  mutation DeleteESM($input: DeleteESMInput!) {
+    deleteESM(input: $input) {
+      Data
+      PK
+      SK
+    }
+  }
+`;
